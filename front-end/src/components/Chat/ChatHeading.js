ada53e0e816389df1ed7349b8e6ff04a568e994a
drew23c <andrewcaldwell@ac.c4q.nyc>
2018-03-25 16:59:59 -0400

    added users online in the chat header

---
+++ front-end/src/components/Chat/ChatHeading.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import FAVideo from 'react-icons/lib/fa/video-camera'
+import FAUserPlus from 'react-icons/lib/fa/user-plus'
+import MdEllipsisMenu from 'react-icons/lib/md/keyboard-control'
+
+export default function({name, numberOfUsers, usersOnline}) {
+	
+	return (
+		<div className="chat-header">
+			<div className="user-info">
+				<div className="user-name">{name}</div>{" "}
+				<div className="users-online"><b>{usersOnline}</b></div>
+				<div className="status">
+					<div className="indicator"></div>
+					<span>{numberOfUsers ? numberOfUsers : null}</span>
+				</div>
+			</div>
+			<div className="options">
+				<FAVideo />
+				<FAUserPlus />
+				<MdEllipsisMenu />
+			</div>
+		</div>
+	);
+	
+}
