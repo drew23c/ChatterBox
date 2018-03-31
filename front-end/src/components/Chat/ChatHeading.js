56be8207e0841dcef3d2784e5c4100e6258e50f2
Susana Han <susanahan@ac.c4q.nyc>
2018-03-31 12:26:14 -0400

    changing styling for chatpage and adding rooms/users leaving and entering on socket

--- front-end/src/components/Chat/ChatHeading.js
+++ front-end/src/components/Chat/ChatHeading.js
@@ -4,7 +4,7 @@ import FAUserPlus from 'react-icons/lib/fa/user-plus'
 import MdEllipsisMenu from 'react-icons/lib/md/keyboard-control'
 import MdEject from 'react-icons/lib/md/eject'
 
-export default function({name, numberOfUsers, usersOnline, logout}) {
+export default function({name, numberOfUsers, usersOnline, logout, user, show}) {
 	return (
 		<div className="chat-header">
 			<div className="user-info">
@@ -15,6 +15,7 @@ export default function({name, numberOfUsers, usersOnline, logout}) {
 					<span>{numberOfUsers ? numberOfUsers : null}</span>
 				</div>
 			</div>
+			<div className="user-show" id="userShow">{show}</div>
 			<div>
 			<div onClick={()=>{logout()}} title="Logout" className="logout">
 			<MdEject/>	
