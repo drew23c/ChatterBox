677e456ce61d9704cc66543fda17c6714cbbda9a
Susana Han <susanahan@ac.c4q.nyc>
2018-05-06 11:34:28 -0400

    heroku deploy

--- back-end/app.js
+++ back-end/app.js
@@ -27,7 +27,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 io.on('connection', SocketManager);
 
@@ -45,6 +46,10 @@ app.use('/episodes', episodes);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
 // error handler
